96ee8d39e9351c01bf476e0a7ab8178c84bed3a1
Ali Hashemi <mr.ali.haashemi@gmail.com>
2023-11-07 02:11:03 +0330

    chore: update featured slides

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,9 +3,10 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async () => {
 	return {
 		slides: [
+			{ text: 'RANDOM\nTEXT', videoSrc: 'x2.mp4' },
 			{ text: 'XXX', videoSrc: 'x1.mp4' },
-			{ text: 'YYY', videoSrc: 'x2.mp4' },
-			{ text: 'ZZZ', videoSrc: 'x3.mp4' },
+			{ text: 'YYY', videoSrc: 'x3.mp4' },
+			{ text: 'ZZZ', videoSrc: 'x4.mp4' },
 		],
 	};
 };
