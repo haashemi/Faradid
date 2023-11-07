5ce05a0adb32c31e6c69f6811fea9eb23c3943a8
Ali Hashemi <mr.ali.haashemi@gmail.com>
2023-11-08 00:32:46 +0330

    chore: add some animations on SideBar

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -1,11 +1,11 @@
 <aside
-	class="hidden h-screen w-14 shrink-0 items-center justify-between bg-black/60 py-10 backdrop-blur-lg md:flex"
+	class="hidden h-screen w-14 shrink-0 items-center justify-between bg-black py-10 backdrop-blur-lg md:flex"
 	style="writing-mode: vertical-lr"
 >
-	<div class="flex gap-7">
-		<a href="/">نمونه کار</a>
-		<a href="/contact">ارتباط با ما</a>
-	</div>
+	<a href="/" class="font-logo text-xl font-bold transition-all duration-300 ease-in-out hover:-translate-y-2 hover:scale-110">فرادید</a>
 
-	<a href="/" class="font-logo text-xl font-bold">فرادید</a>
+	<div class="flex gap-10">
+		<a class="transition-[transform,opacity] duration-300 ease-in-out hover:translate-x-1 hover:opacity-60" href="/contact">ارتباط با ما</a>
+		<a class="transition-[transform,opacity] duration-300 ease-in-out hover:translate-x-1 hover:opacity-60" href="/">نمونه کار</a>
+	</div>
 </aside>
