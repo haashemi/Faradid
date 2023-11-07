998efee768d097ebe48a39c7f94cc1c3df6bd5a3
Ali Hashemi <mr.ali.haashemi@gmail.com>
2023-11-07 19:39:13 +0330

    feat: add sidebar component for desktop

---
+++ src/lib/SideBar.svelte
@@ -0,0 +1,11 @@
+<aside
+	class="absolute right-0 top-0 z-20 hidden h-screen w-14 shrink-0 items-center justify-between bg-black/60 py-10 backdrop-blur-lg md:flex"
+	style="writing-mode: vertical-lr"
+>
+	<div class="flex gap-7">
+		<a href="/">نمونه کار</a>
+		<a href="/contact">ارتباط با ما</a>
+	</div>
+
+	<a href="/" class="font-logo text-xl font-bold">فرادید</a>
+</aside>
