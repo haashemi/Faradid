9a3393b8cf1fb38138fa624607140ecd597530ce
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-06-02 01:34:57 +0330

    feat: add a placeholder for projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,2 +1,10 @@
 <script lang="ts">
 </script>
+
+<div dir="ltr" class="flex w-full flex-col items-center justify-center">
+	<p>NOT IMPLEMENTED</p>
+
+	<div class="mt-10 flex flex-col items-center gap-2">
+		<a href="/">back to Home</a>
+	</div>
+</div>
